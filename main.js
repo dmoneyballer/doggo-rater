62b711556563767aaf279518fe7374df7fdfbcf4
dmoneyballer <drew.gilliesbc@gmail.com>
2019-04-24 14:16:51 -0600

    tidyied html and js

--- main.js
+++ main.js
@@ -1,45 +1,46 @@
 $(document).ready(function() {
-  if(localStorage.getItem('rating') == null) {
+  if (localStorage.getItem("rating") == null) {
     var a = [];
-    a.push(JSON.parse(localStorage.getItem('session')));
-    localStorage.setItem('rating', JSON.stringify(a));
+    a.push(JSON.parse(localStorage.getItem("session")));
+    localStorage.setItem("rating", JSON.stringify(a));
   }
   function getDog() {
-    $("#success").html(``)
-    let url = `https://dog.ceo/api/breeds/image/random`
-    let breed = $('#breed').val().trim();
-    if (breed !== 'select one') {
-      url = `https://dog.ceo/api/breed/${breed}/images/random`
+    $("#success").html(``);
+    let url = `https://dog.ceo/api/breeds/image/random`;
+    let breed = $("#breed")
+      .val()
+      .trim();
+    if (breed !== "select one") {
+      url = `https://dog.ceo/api/breed/${breed}/images/random`;
     }
     $.ajax({
       url: url,
-      type: 'GET',
+      type: "GET",
       success: data => {
-        $('#dogPic').html(`<img src="${data.message}" id="image">`)
-        console.log(data)
+        $("#dogPic").html(`<img src="${data.message}" id="image">`);
+        console.log(data);
       },
       error: err => {
-        console.log(err)
+        console.log(err);
       }
-    })
+    });
   }
-  getDog()
-  $('#doggo').click(function () {
-    getDog()
-  })
+  getDog();
+  $("#doggo").click(function() {
+    getDog();
+  });
 
   $("#rating").click(function() {
-    let image = $("#image")[0].src
-    let rating = $("#ratings").val()
+    let image = $("#image")[0].src;
+    let rating = $("#ratings").val();
     let storage = [];
-    if (rating !== 'select one') {
-      storage = JSON.parse(localStorage.getItem('rating'));
-      storage.push({image: image, rating: rating});
-      localStorage.setItem('rating', JSON.stringify(storage))
-      $('#success').html(`<h1>Rating succesfully updated</h1>`)
+    if (rating !== "select one") {
+      storage = JSON.parse(localStorage.getItem("rating"));
+      storage.push({ image: image, rating: rating });
+      localStorage.setItem("rating", JSON.stringify(storage));
+      $("#success").html(`<h1>Rating succesfully updated</h1>`);
     } else {
-      $('#success').html(`<h1>You need to select a rating.</h1>`)
+      $("#success").html(`<h1>You need to select a rating.</h1>`);
     }
-
-  })
-})
+  });
+});
--- rating.js
+++ rating.js
@@ -1,12 +1,14 @@
-if(localStorage.getItem('rating') !== null) {
-  let ratings = JSON.parse(localStorage.rating)
+if (localStorage.getItem("rating") !== null) {
+  let ratings = JSON.parse(localStorage.rating);
   for (var i = 1; i < ratings.length; i++) {
-    let rating = ratings[i].rating
-    let image = ratings[i].image
-    $('.localStorage').append(`<div class="card card-custom mx-2 mb-3" style="width: 13rem;"><img src=${image}><h1>Rating: ${rating}</h1></div>`)
+    let rating = ratings[i].rating;
+    let image = ratings[i].image;
+    $(".localStorage").append(
+      `<div class="card card-custom mx-2 mb-3" style="width: 13rem;"><img src=${image}><h1>Rating: ${rating}</h1></div>`
+    );
   }
 }
-$('.btn-danger').click(function () {
+$(".btn-danger").click(function() {
   localStorage.clear();
-  $('.localStorage').html(`<h1>All ratings have been deleted.</h1>`)
-})
+  $(".localStorage").html(`<h1>All ratings have been deleted.</h1>`);
+});
