418a0fab6e1215a3fb38451a4ca11d9a1bcd10af
dmoneyballer <drew.gilliesbc@gmail.com>
2019-04-24 10:04:42 -0600

    better styled and more functional

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     localStorage.setItem('rating', JSON.stringify(a));
   }
   function getDog() {
+    $("#success").html(``)
     let url = `https://dog.ceo/api/breeds/image/random`
     let breed = $('#breed').val().trim();
     if (breed !== 'select one') {
@@ -31,9 +32,14 @@ $(document).ready(function() {
     let image = $("#image")[0].src
     let rating = $("#ratings").val()
     let storage = [];
-    storage = JSON.parse(localStorage.getItem('rating'));
-    storage.push({image: image, rating: rating});
-    localStorage.setItem('rating', JSON.stringify(storage))
-    $('#success').html(`<h1>Rating succesfully updated</h1>`)
+    if (rating !== 'select one') {
+      storage = JSON.parse(localStorage.getItem('rating'));
+      storage.push({image: image, rating: rating});
+      localStorage.setItem('rating', JSON.stringify(storage))
+      $('#success').html(`<h1>Rating succesfully updated</h1>`)
+    } else {
+      $('#success').html(`<h1>You need to select a rating.</h1>`)
+    }
+
   })
 })
--- rating.js
+++ rating.js
@@ -3,6 +3,10 @@ if(localStorage.getItem('rating') !== null) {
   for (var i = 1; i < ratings.length; i++) {
     let rating = ratings[i].rating
     let image = ratings[i].image
-    $('.localStorage').append(`<img src=${image}><h1>${rating}</h1>`)
+    $('.localStorage').append(`<div class="card card-custom mx-2 mb-3" style="width: 13rem;"><img src=${image}><h1>Rating: ${rating}</h1></div>`)
   }
 }
+$('.btn-danger').click(function () {
+  localStorage.clear();
+  $('.localStorage').html(`<h1>All ratings have been deleted.</h1>`)
+})
